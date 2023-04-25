4f0a4653f7bc9fa73ea891ff0af42b0924a7d6df
Valentino <valentinosetiawan32@gmail.com>
2023-04-25 15:55:16 +0700

    Fixed Username Missing

--- app/src/main/java/edu/bluejack22_2/BeeTech/ReviewDetailActivity.kt
+++ app/src/main/java/edu/bluejack22_2/BeeTech/ReviewDetailActivity.kt
@@ -2,10 +2,34 @@ package edu.bluejack22_2.BeeTech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import edu.bluejack22_2.BeeTech.databinding.ActivityMainBinding
+import edu.bluejack22_2.BeeTech.databinding.ActivityReviewDetailBinding
+import util.ActivityTemplate
 
-class ReviewDetailActivity : AppCompatActivity() {
+class ReviewDetailActivity : AppCompatActivity(), ActivityTemplate {
+    lateinit var binding: ActivityReviewDetailBinding
+    lateinit var imageView: ImageView
+    lateinit var title: TextView
+    lateinit var createdAt: TextView
+    lateinit var author: TextView
+    lateinit var favCount: TextView
+    lateinit var favorite: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review_detail)
+        init()
+        onAction()
+    }
+
+    override fun init() {
+        binding = ActivityReviewDetailBinding.inflate(layoutInflater)
+
+    }
+
+    override fun onAction() {
+
     }
 }
